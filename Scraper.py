d10d6a926dbada19d90edb06e99e40d8fdacda06
Kevin Leroux <k.lrx076@gmail.com>
2022-06-15 11:02:12 +0200

    Retrieve data

--- Scraper.py
+++ Scraper.py
@@ -1,7 +1,15 @@
-from requests import request
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.service import Service as FirefoxService
+import os
+from urllib import request
 
 
+import time
+
+
+# find link element 
+
+# get download link
+download_link='https://www.data.gouv.fr/fr/datasets/r/8597b9e5-162b-472a-a27e-fe1b25eb0c35'
+
+destination_filename=os.path.join(os.curdir,"Candidates_data.xlsx")
+response=request.urlretrieve(download_link,destination_filename)
+time.sleep(3)
