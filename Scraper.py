ae109bfb38f9abde3bd469debc461e9d378abf5c
Kevin Leroux <k.lrx076@gmail.com>
2022-06-15 10:26:50 +0200

    Imports

---
+++ Scraper.py
@@ -0,0 +1,7 @@
+from requests import request
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+
+
